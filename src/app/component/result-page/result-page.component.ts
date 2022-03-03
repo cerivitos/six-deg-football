3bd1f19a748196e8ab5793cbf68c8fe565762feb
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-03-03 21:50:00 +0800

    Add mock data to test result page

--- src/app/component/result-page/result-page.component.ts
+++ src/app/component/result-page/result-page.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Player } from 'src/app/model/Player';
 import { Team } from 'src/app/model/Team';
 import { GameControllerService } from 'src/app/service/game-controller.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { convertSec } from 'src/app/util/convertSec';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-result-page',
@@ -19,10 +20,49 @@ export class ResultPageComponent implements OnInit {
   constructor(private gameControllerService: GameControllerService) {}
 
   ngOnInit(): void {
-    this.steps$ = this.gameControllerService.steps$;
-    this.time$ = this.gameControllerService.time$;
-    this.teamHistory$ = this.gameControllerService.teamHistory$;
-    this.playerHistory$ = this.gameControllerService.playerHistory$;
+    if (environment.production) {
+      this.steps$ = this.gameControllerService.steps$;
+      this.time$ = this.gameControllerService.time$;
+      this.teamHistory$ = this.gameControllerService.teamHistory$;
+      this.playerHistory$ = this.gameControllerService.playerHistory$;
+    } else {
+      this.steps$ = new BehaviorSubject<number>(13);
+      this.time$ = new BehaviorSubject<number>(240);
+
+      let testTeamHistory: Team[] = [];
+      this._populateTestTeamData().then((data) => {
+        testTeamHistory = data;
+        this.teamHistory$ = new BehaviorSubject<Team[]>(testTeamHistory);
+      });
+
+      let testPlayerHistory: Player[] = [];
+      this._populateTestPlayerData().then((data) => {
+        testPlayerHistory = data;
+        this.playerHistory$ = new BehaviorSubject<Player[]>(testPlayerHistory);
+      });
+    }
+  }
+
+  private async _populateTestTeamData(): Promise<Team[]> {
+    const response = await fetch('../../assets/test-team-history.json');
+    const testTeamHistoryJson = await response.json();
+    let testTeamHistory: Team[] = [];
+    for (const team of testTeamHistoryJson) {
+      testTeamHistory.push(team as Team);
+    }
+
+    return testTeamHistory;
+  }
+
+  private async _populateTestPlayerData(): Promise<Player[]> {
+    const response = await fetch('../../assets/test-player-history.json');
+    const testPlayerHistoryJson = await response.json();
+    let testPlayerHistory: Player[] = [];
+    for (const player of testPlayerHistoryJson) {
+      testPlayerHistory.push(player as Player);
+    }
+
+    return testPlayerHistory;
   }
 
   convertSec(s: number | null): string {
--- src/app/module/result/result-routing.module.ts
+++ src/app/module/result/result-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {
     path: '',
     component: ResultPageComponent,
-    canActivate: [ResultGuard],
+    //canActivate: [ResultGuard],
   },
 ];
 
