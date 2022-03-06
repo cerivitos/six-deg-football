01ff6e4514a7dc30a6eb7ddf2b701a24a764b321
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-03-06 22:58:19 +0800

    Add entry anim

--- src/app/component/player-card/player-card.component.ts
+++ src/app/component/player-card/player-card.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Player } from 'src/app/model/Player';
 
@@ -5,6 +6,17 @@ import { Player } from 'src/app/model/Player';
   selector: 'app-player-card',
   templateUrl: './player-card.component.html',
   styleUrls: ['./player-card.component.css'],
+  animations: [
+    trigger('enterAnim', [
+      transition(':enter', [
+        style({ transform: 'translateY(12px)', opacity: 0.5 }),
+        animate(
+          '80ms ease-in',
+          style({ transform: 'translateY(0)', opacity: 1 })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class PlayerCardComponent implements OnInit {
   @Input() player: Player | undefined | null;
