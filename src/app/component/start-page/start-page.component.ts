c30b7de4003283ed93c8143fce102bd65b0f28b1
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-03-11 17:38:04 +0800

    Add footer

--- src/app/component/start-page/start-page.component.ts
+++ src/app/component/start-page/start-page.component.ts
@@ -1,15 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
-  styleUrls: ['./start-page.component.css']
+  styleUrls: ['./start-page.component.css'],
 })
 export class StartPageComponent implements OnInit {
+  version: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get<string>('/api/query-sha')
+      .pipe(take(1))
+      .subscribe((sha) => (this.version = sha));
   }
-
 }
