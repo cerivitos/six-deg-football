ba3ce86732b1b4e244fc71dd8bcce7b7d8a64c98
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-03-11 17:56:19 +0800

    Implement animation for name changing

--- src/app/component/name-carousel/name-carousel.component.ts
+++ src/app/component/name-carousel/name-carousel.component.ts
@@ -1,9 +1,46 @@
+import {
+  animate,
+  keyframes,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-name-carousel',
   templateUrl: './name-carousel.component.html',
   styleUrls: ['./name-carousel.component.css'],
+  animations: [
+    trigger('name1Anim', [
+      transition('* => *', [
+        style({ opacity: 0 }),
+        animate(
+          '1500ms',
+          keyframes([
+            style({ opacity: 0, offset: 0 }),
+            style({ opacity: 1, offset: 0.1 }),
+            style({ opacity: 1, offset: 0.9 }),
+            style({ opacity: 0, offset: 1 }),
+          ])
+        ),
+      ]),
+    ]),
+    trigger('name2Anim', [
+      transition('* => *', [
+        style({ opacity: 0 }),
+        animate(
+          '2000ms',
+          keyframes([
+            style({ opacity: 0, offset: 0 }),
+            style({ opacity: 1, offset: 0.1 }),
+            style({ opacity: 1, offset: 0.9 }),
+            style({ opacity: 0, offset: 1 }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class NameCarouselComponent implements OnInit {
   constructor() {}
@@ -68,7 +105,7 @@ export class NameCarouselComponent implements OnInit {
       }
 
       this.name1 = this.names[index1];
-    }, 2000);
+    }, 1500);
 
     setInterval(() => {
       if (index2 + 1 < this.names.length) {
@@ -81,6 +118,6 @@ export class NameCarouselComponent implements OnInit {
         index2 = 0;
       }
       this.name2 = this.names[index2];
-    }, 1500);
+    }, 2000);
   }
 }
